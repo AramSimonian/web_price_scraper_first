35d9f6ccb250f7e753743e2ad6f5cf0e854bd41a
Aram Simonian <aram.contacts@gmail.com>
2020-03-18 00:50:10 +0000

    Altered the pause to 3 seconds

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-timeout = 5
+timeout = 3
 
 sains = {
     'name': 'Sains',
