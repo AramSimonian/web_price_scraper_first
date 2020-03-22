4ffe0cbf89c39e73b2d0c3fb4eac7f871f945af9
Aram Simonian <aram.contacts@gmail.com>
2020-03-22 23:51:52 +0000

    Added module-level variables; refactored to allow easier testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib3
 import re
+import sys
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -8,6 +9,10 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
+this = sys.modules[__name__]
+this.all_tags = {}
+this.driver = ''
 
 timeout = 3
 
@@ -54,46 +59,51 @@ def get_shop_from_link(link):
     shop = link.split(".")
     return shop[1]
 
-def get_shop_tags(all_tags, shop):
-    pause_for_class_name = all_tags[shop]['pause_for_class_name']
-    tag_type = all_tags[shop]['tag_type']
-    tag_attr = all_tags[shop]['tag_attr']
-    class_name = all_tags[shop]['class_name']
+def get_shop_tags(shop):
+    pause_for_class_name = this.all_tags[shop]['pause_for_class_name']
+    tag_type = this.all_tags[shop]['tag_type']
+    tag_attr = this.all_tags[shop]['tag_attr']
+    class_name = this.all_tags[shop]['class_name']
 
     return pause_for_class_name, tag_type, tag_attr, class_name
 
-def main():
-    all_tags = build_supermarket_tags()
+def extract_price_wrapper_from_link(product_link, shop):
+    # retrieve the tag info for the shop
+    pause_for_class_name, tag_type, tag_attr, class_name = get_shop_tags(shop)
 
-    driver = webdriver.Chrome()
+    # navigate to the grocery item site
+    this.driver.get(product_link)
+
+    element_present = EC.presence_of_element_located((By.CLASS_NAME, pause_for_class_name))
+    WebDriverWait(this.driver, timeout).until(element_present)
+    soup = BeautifulSoup(this.driver.page_source, 'html.parser')
+    return soup.find(tag_type, attrs={tag_attr: class_name})
+
+def get_shop_and_price_from_link(product_link):
+    # determine the shop
+    shop = get_shop_from_link(product_link)
+
+    try:
+        price_wrapper = extract_price_wrapper_from_link(product_link, shop)
+        if price_wrapper is None:
+            price = 'Unable to locate price wrapper'
+        else:
+            price = '£' + re.findall(r'(\d+\.\d{2}|\d+)', str(price_wrapper))[0]
+    except TimeoutException:
+        price = 'Unable to retrieve price - timeout'
+
+    return "{0}: {1}\n".format(shop.title(), price)
+
+
+def main():
+    this.all_tags = build_supermarket_tags()
+
+    this.driver = webdriver.Chrome()
 
     output = 'Bagels:\n'
 
     for bagel_link in nybagel_links:
-        try:
-            # determine the shop
-            shop = get_shop_from_link(bagel_link)
-
-            # retrieve the tag info for the shop
-            pause_for_class_name, tag_type, tag_attr, class_name = get_shop_tags(all_tags, shop)
-
-            # navigate to the grocery item site
-            driver.get(bagel_link)
-
-            element_present = EC.presence_of_element_located((By.CLASS_NAME, pause_for_class_name))
-            WebDriverWait(driver, timeout).until(element_present)
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            price_box = soup.find(tag_type, attrs={tag_attr: class_name})
-
-            if price_box is None:
-                price = ""
-            else:
-                price = '£' + re.findall(r'(\d+\.\d{2}|\d+)', str(price_box))[0]
-
-        except TimeoutException:
-            price = 'Unable to retrieve price'
-        finally:
-            output += "{0}: {1}\n".format(shop.title(), price)
+        output += get_shop_and_price_from_link(bagel_link)
 
     with open("C:\\Temp\\price_check.txt", "w") as file:
         file.write(output)
