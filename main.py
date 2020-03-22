02bbc29efc13a624498f1bb29510cebf31da4488
Aram Simonian <aram.contacts@gmail.com>
2020-03-22 01:03:23 +0000

    Started creating test data; refactored code to separate tag info & links; implemented functions

--- main.py
+++ main.py
@@ -11,91 +11,92 @@ from selenium.webdriver.common.by import By
 
 timeout = 3
 
-sains = {
-    'name': 'Sains',
-    'site': 'https://www.sainsburys.co.uk/shop/gb/groceries/product/details/new-york-bakery-co-cinnamon---raisin-bagels-x5',
-    'tag_type': 'div',
-    'tag_id': 'pd-retail-price',
-    'tag_attr': 'class',
-    'class_name': 'pd__cost__total--promo undefined',
-    'pause_for_class_name': 'pd__cost__per-unit'
-    }
-morrisons = {
-    'name': 'Morrisons',
-    'site': 'https://groceries.morrisons.com/webshop/product/New-York-Bagel-Co-Cinnamon--Raisin/114353011',
-    'tag_type': 'meta',
-    'tag_id': '',
-    'tag_attr': 'itemprop',
-    'class_name': 'price',
-    'pause_for_class_name': 'bop-price__wrapper'
-    }
-asda = {
-    'name': 'Asda',
-    'site': 'https://groceries.asda.com/product/bagels/new-york-bakery-co-cinnamon-raisin-bagels/1000004372338',
-    'tag_type': 'strong',
-    'tag_id': '',
-    'tag_attr': 'class',
-    'class_name': 'co-product__price pdp-main-details__price',
-    'pause_for_class_name': 'pdp-main-details__price-container'
-    }
-tesco = {
-    'name': 'Tesco',
-    'site': 'https://www.tesco.com/groceries/en-GB/products/253829047',
-    'tag_type': 'div',
-    'tag_id': '',
-    'tag_attr': 'class',
-    'class_name': 'price-per-sellable-unit price-per-sellable-unit--price price-per-sellable-unit--price-per-item',
-    'pause_for_class_name': 'price-details--wrapper'
-    }
+nybagel_links = ['https://www.sainsburys.co.uk/shop/gb/groceries/product/details/new-york-bakery-co-cinnamon---raisin-bagels-x5',
+               'https://groceries.morrisons.com/webshop/product/New-York-Bagel-Co-Cinnamon--Raisin/114353011',
+               'https://groceries.asda.com/product/bagels/new-york-bakery-co-cinnamon-raisin-bagels/1000004372338',
+               'https://www.tesco.com/groceries/en-GB/products/253829047']
 
-#sains
-#<div data-test-id="pd-retail-price" class="pd__cost__total--promo undefined">£1.20</div>
-##asda
-#<strong class="co-product__price pdp-main-details__price">£1.00</strong>
-#tesco
-#https://www.tesco.com/groceries/en-GB/products/253829047
-#<span data-auto="price-value" class="value">1.00</span>
+def build_supermarket_tags():
+    sainsburys_tags = {
+        'tag_type': 'div',
+        'tag_id': 'pd-retail-price',
+        'tag_attr': 'class',
+        'class_name': 'pd__cost__total--promo undefined',
+        'pause_for_class_name': 'pd__cost__per-unit'
+        }
+    morrisons_tags = {
+        'tag_type': 'meta',
+        'tag_id': '',
+        'tag_attr': 'itemprop',
+        'class_name': 'price',
+        'pause_for_class_name': 'bop-price__wrapper'
+        }
+    asda_tags = {
+        'tag_type': 'strong',
+        'tag_id': '',
+        'tag_attr': 'class',
+        'class_name': 'co-product__price pdp-main-details__price',
+        'pause_for_class_name': 'pdp-main-details__price-container'
+        }
+    tesco_tags = {
+        'tag_type': 'div',
+        'tag_id': '',
+        'tag_attr': 'class',
+        'class_name': 'price-per-sellable-unit price-per-sellable-unit--price price-per-sellable-unit--price-per-item',
+        'pause_for_class_name': 'price-details--wrapper'
+        }
 
-bagels = [sains, morrisons, asda, tesco]
+    output = {
+        'sainsburys': sainsburys_tags,
+        'morrisons': morrisons_tags,
+        'asda': asda_tags,
+        'tesco': tesco_tags
+        }
+    return output
 
-driver = webdriver.Chrome()
+def get_shop_from_link(link):
+    shop = link.split(".")
+    return shop[1]
 
-output = 'Bagels:\n'
+def main():
+    all_tags = build_supermarket_tags()
 
-for bagel in bagels:
-    driver.get(bagel['site'])
+    driver = webdriver.Chrome()
 
-    try:
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, bagel['pause_for_class_name']))
-        WebDriverWait(driver, timeout).until(element_present)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        price_box = soup.find(bagel['tag_type'], attrs={bagel['tag_attr']: bagel['class_name']})
+    output = 'Bagels:\n'
 
-        if price_box is None:
-            price = ""
-        else:
-            price = re.findall(r'(\d+\.\d{2}|\d+)', str(price_box))[0]
+    for bagel_link in nybagel_links:
+        # navigate to the grocery item site
+        driver.get(bagel_link)
 
-    except TimeoutException:
-        price = 'Unable to retrieve price'
-    finally:
-        output += "{0}: £{1}\n".format(bagel['name'], price)
+        # determine the shop and retrieve the tag info
+        shop = get_shop_from_link(bagel_link)
 
-with open("C:\\Temp\\price_check.txt", "w") as file:
-    file.write(output)
+        pause_for_class_name = all_tags[shop]['pause_for_class_name']
+        tag_type = all_tags[shop]['tag_type']
+        tag_attr = all_tags[shop]['tag_attr']
+        class_name = all_tags[shop]['class_name']
 
-#sains
-#<div data-test-id="pd-retail-price" class="pd__cost__total--promo undefined">£1.20</div>
-#price_box = soup.find('div', attrs={'class': 'pd__cost__total--promo undefined'})
-#<div class="pd__cost__total--promo undefined" data-test-id="pd-retail-price">£1.20</div>
 
-#morrisons
-#<span class="nowPrice"> £1 </span>
-#price_box = soup.find('span', attrs={'class': 'nowPrice'})
-#<meta itemprop="price" content="0.77">
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, pause_for_class_name))
+            WebDriverWait(driver, timeout).until(element_present)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            price_box = soup.find(tag_type, attrs={tag_attr: class_name})
 
-#price = price_box.text.strip()
-#print("Sainsbury's bagels: " + price)
-#print("Morrisons bagels: " + price)
-driver.quit()
+            if price_box is None:
+                price = ""
+            else:
+                price = re.findall(r'(\d+\.\d{2}|\d+)', str(price_box))[0]
 
+        except TimeoutException:
+            price = 'Unable to retrieve price'
+        finally:
+            output += "{0}: £{1}\n".format(shop.title(), price)
+
+    with open("C:\\Temp\\price_check.txt", "w") as file:
+        file.write(output)
+
+    driver.quit()
+
+main()
