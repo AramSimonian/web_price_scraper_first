752aa204fade4b61326c6c6b1bab175fcad06f8e
Aram Simonian <aram.contacts@gmail.com>
2020-07-28 20:58:17 +0100

    Modified main() to add arguments to ease Docker implementation

--- main.py
+++ main.py
@@ -229,7 +229,13 @@ def write_output(price_list):
 def main():
     this.all_tags = build_supermarket_attribs()
 
-    this.driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window_size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+
+    this.driver = webdriver.Chrome(options=chrome_options)
 
     output = ''
     product_wrapper = ProductWrapper()
