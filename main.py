2b19fc56ac8e081c3de057b65ea9316ee6a1b9c4
Aram Simonian <aram.contacts@gmail.com>
2022-01-26 22:09:53 +0000

    Add explanatory note about chromedriver headless option

--- main.py
+++ main.py
@@ -272,8 +272,10 @@ def main():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window_size=1420,1080')
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    
+    # Enabling this option blocks Sainsbury's prices
+    #chrome_options.add_argument('--headless')
 
     this_os = platform.system()
     if this_os == 'Linux':
